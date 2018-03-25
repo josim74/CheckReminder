8dd7b74c45e50f278ac1c82c73f57348f072a20f
Md. Morshadun Nur <13203016@iubat.edu>
2018-03-25 17:52:21 +0600

    Initial commit

---
+++ app/src/main/java/softtech/smg/com/checkreminder/CheckInputActivity.java
@@ -0,0 +1,13 @@
+package softtech.smg.com.checkreminder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CheckInputActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_check_input);
+    }
+}
---
+++ app/src/main/java/softtech/smg/com/checkreminder/HomeActivity.java
@@ -0,0 +1,50 @@
+package softtech.smg.com.checkreminder;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class HomeActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    fragmentTransaction.replace(R.id.content_main,new RecievableFragment()).commit();
+                    return true;
+                case R.id.navigation_dashboard:
+                    fragmentTransaction.replace(R.id.content_main,new PayableFragment()).commit();
+                    return true;
+                case R.id.navigation_notifications:
+                    Intent checkInputIntent = new Intent(HomeActivity.this, CheckInputActivity.class);
+                    startActivity(checkInputIntent);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
